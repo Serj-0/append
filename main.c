066e8b10463a4ce06a1d60ca50dc93a2a54af919
Serj-0 <skyperwiz@tutanota.com>
2021-12-06 03:10:43 -0500

    small cleanup

--- main.c
+++ main.c
@@ -2,12 +2,10 @@
 #include <string.h>
 typedef unsigned char bool;
 
-char str[BUFSIZ];
-char out[BUFSIZ];
+static inline void print_append(char* src, char** app, int n, bool front);
 
-void print_append(char* src, char** app, int n, bool front);
 void usage(){
-    printf("%s", "app -[fnNs] [FILE] [STRING...]");
+    printf("%s", "app -[fnNs] [FILE] [STRING...]\n");
 }
 
 int main(int argc, char** args){
@@ -17,6 +15,11 @@ int main(int argc, char** args){
             seqnc = 0;      // append string args in a sequence
     
     /* FLAGS */
+    if(argc < 2){
+        usage();
+        return 0;
+    }
+    
     if(args[1][0] == '-'){
         for(int j = 1; j < strlen(args[1]); j++){
             switch(args[1][j]){
@@ -32,17 +35,23 @@ int main(int argc, char** args){
             case 's':
                 seqnc = 1;
                 break;
+            case 'h':
+                usage();
+                return 0;
             }
         }
     }
     
-    //if first arg did not switch any flags, treat as append string
+    char str[BUFSIZ];
+    
+    //if first was a flag switch, move arg index forward one
     int aas = (front || nline || Nline || seqnc);
     
     /* FILE */
     FILE* ist = fopen(args[1 + aas], "r");
     if(!ist) ist = stdin;
     
+    //if first or second are a file, move arg index forward one
     aas += (ist != stdin);
     
     /* OPERATION */
@@ -66,8 +75,11 @@ int main(int argc, char** args){
     return 0;
 }
 
-void print_append(char* src, char** app, int n, bool front){
-    if(!front) strncpy(out + strlen(out), src, strlen(src));
+/*************************************************************************************************************/
+
+static inline void print_append(char* src, char** app, int n, bool front){
+    char out[BUFSIZ];
+    if(!front) strncpy(out + BUFSIZ, src, strlen(src));
     for(int i = 0; i < n; i++){
                                 //v~~~~~~~~~~~~~~v if is first string arg, and front flag is false, move back one more character
         strncpy(out + strlen(out) - (!i && !front), app[i], strlen(app[i]));
